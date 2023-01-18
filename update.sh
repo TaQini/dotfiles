a89b2ff6805fb8e60ca10f7233c95918d7b6e20a
TaQini <742954809@qq.com>
2023-01-18 20:01:56 +0800

    add a script to update conf

---
+++ update.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+rm -rf hyprland/.config/hypr && cp -r ~/.config/hypr hyprland/.config
+rm -rf fuzzel/.config/fuzzel && cp -r ~/.config/fuzzel fuzzel/.config
+rm -rf foot/.config/foot && cp -r ~/.config/foot foot/.config
+rm -rf fontconfig/.config/fontconfig && cp -r ~/.config/fontconfig fontconfig/.config
