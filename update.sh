d5217bc831d8abcdafb5739d87654f4a49ad881e
TaQini <742954809@qq.com>
2023-01-19 09:34:09 +0800

    add wlogout

--- update.sh
+++ update.sh
@@ -6,3 +6,4 @@ rm -rf foot/.config/foot && cp -r ~/.config/foot foot/.config
 rm -rf fontconfig/.config/fontconfig && cp -r ~/.config/fontconfig fontconfig/.config
 rm -rf scripts/Workspace/Scripts && cp -r ~/scripts/Workspace/Scripts scripts/Workspace
 rm -rf swaylock/.config/swaylock && cp -r ~/.config/swaylock swaylock/.config
+rm -rf wlogout/.config/wlogout && cp -r ~/.config/wlogout wlogout/.config
